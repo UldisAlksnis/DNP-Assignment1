9d3297f048959c62a5a92771ecbcea5589e0a02f
Uldis Alksnis <csmccs1@gmail.com>
2020-10-08 14:13:44 +0200

    Authentication_v1

---
+++ Assignment1/Models/Authorization/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models.Authorization
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Domain { get; set; }
+        public string City { get; set; }
+        public int BirthYear { get; set; }
+        public string Role { get; set; }
+        public int SecurityLevel { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
